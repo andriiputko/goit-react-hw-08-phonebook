7a1d06bbe0c1d75c1d7952aead8f0f75d0ad98fc
Andrii <andriiputko@gmail.com>
2023-09-24 15:44:27 +0200

    Try to use Chakra

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import { App } from './components/App';
 import './index.css';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-       <App />
-      </Provider>
+    <ChakraProvider>
+      <BrowserRouter>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <App />
+          </PersistGate>
+        </Provider>
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
