37278986505dca899cdef119ddd7e2eb5d2bb52f
Andrii <andriiputko@gmail.com>
2023-09-24 20:06:43 +0200

    Try to fix deploy error

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-      <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+      <BrowserRouter>
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
             <App />
